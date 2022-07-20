76cd802afa8ced9bf21c5c9a0ea1f409fae52e54
guadrivmart <gust_19@hotmail.com>
2022-07-19 21:30:43 -0500

    new modific

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -223,7 +223,7 @@ function esPositivo(numero) {
     {
       print("Es positivo");
     }
-  else if{
+  else if {
     return false;
   }
 
@@ -242,7 +242,7 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  console.log("nombre"+"apellido");
+  console.log("nombre", "apellido");
 }
 
 function obtenerSaludo(nombre) {
@@ -255,20 +255,24 @@ function obtenerSaludo(nombre) {
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+var area=alto*ancho;
+return area;
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  var perim=lado*4;
+  return perim;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
+  var area=(base*altura)/2;
+  return area;
 
 }
 
@@ -277,7 +281,8 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  var dolar=euro*1.20;
+  return dolar;
 }
 
 
